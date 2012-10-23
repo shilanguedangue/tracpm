8d4ebe27609692ab0f3e304efd6449089cf0f0e4
kevin.j.fox <kevin.j.fox@gmail.com>
2012-10-23 01:57:40 +0000

    Removed changeset option, future work... Updated beta version 0.3.1

--- TracPM/setup.py
+++ TracPM/setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='TracPM',
-    version='0.2.0',
+    version='0.3.1',
     packages=['tracpm'],
     author='Kevin Fox',
     description='Project Planning / scheduling / charting....',
